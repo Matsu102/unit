549f43020a6dac31df6a935a360f47ae9b613b16
Matsu102 <124568793+Matsu102@users.noreply.github.com>
2023-05-21 12:21:43 +0000

    [Save]①3つのgemの導入とインストール※詳細はGemfileを参照。②ホスト許可。③deviseで管理者のLog_inと利用者のLog_in,Sign_upのviewとcontrollerを作成。④テーブル定義書の1_users,2_adminsをマイグレート。"

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+# 管理者用
+# /users/sign_in...
+devise_for :admin, skip: [:registrations, :passwords] , controllers: {
+  sessions: "admin/sessions"
+}
+
+
+# 利用者用
+# /admin/sign_in...
+devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
