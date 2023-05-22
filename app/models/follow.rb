eac3ce40e27c4a57f35dbd3f9455c22fd364e231
Matsu102 <124568793+Matsu102@users.noreply.github.com>
2023-05-22 15:09:36 +0000

    [Save]モデルのアソシエーション(User)-(Follow)と(Post)-(PostTag)-(Tag)を定義。

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,6 @@
 class Follow < ApplicationRecord
+
+  belongs_to :follower, class_name: "User" # follower == Userモデルのid
+  belongs_to :followed, class_name: "User" # followed == Userモデルのid
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :followers, class_name: "Follow", foreign_key: "follower_id" # followers == Followモデルのfollower_id == フォローしたユーザのid == current_userがフォローしているユーザ
+  has_many :followeds, class_name: "Follow", foreign_key: "followed_id" # followeds == Followモデルのfollowed_id == フォローされたユーザのid == current_userをフォローしているユーザ
+
+  has_many :followings, through: :followers, source: :followed # ユーザのフォローリストで表示   中間テーブル経由でcurrent_userがフォローしたユーザ情報を参照
+  has_many :followers, through: :followeds, source: :follower  # ユーザのフォロワーリストで表示 中間テーブル経由でcurrent_userをフォローしたユーザの情報を参照
+
 end
