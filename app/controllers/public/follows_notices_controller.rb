54e15c898bf5f7cdf7e58252deb5cbbbc3705424
Matsu102 <124568793+Matsu102@users.noreply.github.com>
2023-06-22 00:01:06 +0000

    [Save]通知削除機能実装中(部分削除と一括削除機能完了)。trueを非表示にする必要がある。

--- app/controllers/public/follows_notices_controller.rb
+++ app/controllers/public/follows_notices_controller.rb
@@ -4,18 +4,22 @@ before_action :authenticate_user!
   def index
     user = current_user
     #-----フォロー機能
-    @my_follow = user.my_followers   # フォロー 一覧
-    @my_follower = user.my_followeds # フォロワー 一覧
+    @my_followers = user.my_followers   # フォロー 一覧
+    @my_followeds = user.my_followeds # フォロワー 一覧
     #-----通知機能
     notices = current_user.passive_notices
-    notices.where(checked: false).each do |notice|
-      notice.update_attribute(:checked, true)
-    end
     @notice = notices.where.not(visitor_id: current_user.id)
   end
 
   def update
     @notice = Notice.find(params[:id])
+    @notice.update(notice_params)
+    redirect_to follows_notices_path
+  end
+
+  def update_all
+    notices = current_user.passive_notices
+    @notice = notices.where.not(visitor_id: current_user.id)
     @notice.update(notice_params)
     redirect_to follows_notices_path
   end
