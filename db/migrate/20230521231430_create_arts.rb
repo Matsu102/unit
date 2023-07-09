6de5f5b5da2df75fb7cbc816b67107210110f333
Matsu102 <124568793+Matsu102@users.noreply.github.com>
2023-07-09 15:03:15 +0000

    [Finish-next_test]追加漏れのArt.is_deletedカラムを追加。テストの為に全データ削除。

--- db/migrate/20230521231430_create_arts.rb
+++ db/migrate/20230521231430_create_arts.rb
@@ -7,6 +7,7 @@ class CreateArts < ActiveRecord::Migration[6.1]
       t.integer :user_id, null: false
       t.string  :title,   null: false
       t.text    :detail,  null: false
+      t.boolean :is_deleted, null: false, default: false # 削除ステータス false:公開 true:削除
 
       # 追加カラム ここまで
 
