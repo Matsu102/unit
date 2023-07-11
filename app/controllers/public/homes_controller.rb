4267f97a667fae05c86f83a5db29f35389d2c579
Matsu102 <124568793+Matsu102@users.noreply.github.com>
2023-07-11 12:30:33 +0000

    [Finish]テスト完了。バグ修正済み。

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::HomesController < ApplicationController
 before_action :is_locked_protect
 
   def top
-    @arts = Art.where(is_deleted: false).order(id: :desc)
+    @arts = Art.includes(:user).where(users: {is_deleted: false, is_locked: false}).where(is_deleted: false).order(id: :desc)
   end
 
   def branch
